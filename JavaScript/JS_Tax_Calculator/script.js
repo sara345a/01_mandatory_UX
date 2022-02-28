ba707aa8ae215eb40e13ed8830f052dc882dd61c
Sara Nielsen <sara345a@stud.kea.dk>
2022-02-28 15:14:57 +0100

    tax calculator done (maybe)

---
+++ JavaScript/JS_Tax_Calculator/script.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const calculateTax = (amount, percentage) => {
+  return (amount / 100) * percentage;
+};
+
+const formSumbited = (event) => {
+  event.preventDefault();
+
+  // get values from form
+  const moneratyAmount = event.target.querySelector("#monetary_amount").value;
+  const taxPercentage = event.target.querySelector("#tax_percentage").value;
+
+  // calculate tax and final result
+  const finalTaxResult = calculateTax(moneratyAmount, taxPercentage);
+  const finalResult = moneratyAmount - finalTaxResult;
+
+  // display tax result
+  const taxResultSpan = document.querySelector("#tax_result");
+  taxResultSpan.textContent = finalTaxResult.toFixed(2);
+
+  // display final result
+  const finalResultSpan = document.querySelector("#final_result");
+  finalResultSpan.textContent = finalResult.toFixed(2);
+};
+
+const loadAndWaitForCalculation = () => {
+  // get form and add listener for submit
+  const form = document.querySelector("form");
+  form.addEventListener("submit", (event) => formSumbited(event));
+};
+
+window.addEventListener("load", loadAndWaitForCalculation);
